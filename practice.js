18b72dbbc5de5c5627ef76afc781feb13cc6a227
ZoranUTF8 <janjiczoran23@gmail.com>
2022-12-21 23:02:58 +0100

    add is isogram

--- practice.js
+++ practice.js
@@ -1,22 +1,40 @@
+"use strict";
 //! isPangram
 
-function isPangram(stringToCheck) {
-  let strArr = stringToCheck.toLowerCase();
-  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+// function isPangram(stringToCheck) {
+//   let strArr = stringToCheck.toLowerCase();
+//   let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-  for (let index = 0; index < alphabet.length; index++) {
-    const element = alphabet[index];
-    if (strArr.indexOf(element) < 0) {
-      return false;
-    }
+//   for (let index = 0; index < alphabet.length; index++) {
+//     const element = alphabet[index];
+//     if (strArr.indexOf(element) < 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// const test1 = "The quick brown fox jumps over the lazy dog.";
+// const test2 = "This is not a pangram.";
+
+// let res1 = isPangram(test1);
+// console.log(res1);
+// let res2 = isPangram(test2);
+// console.log(res2);
+
+// ! An isogram is a word with a repeated character.
+function isIsogram(stringToCheck) {
+  const strArr = stringToCheck.toLowerCase();
+
+  const strSet = new Set(strArr);
+
+  if (strSet.size === strArr.length) {
+    return false;
   }
   return true;
 }
 
-const test1 = "The quick brown fox jumps over the lazy dog.";
-const test2 = "This is not a pangram.";
+const res1 = isIsogram("thomas"); /* no repeating letter */
+const res2 = isIsogram("moses"); /* s repeat 2 times */
 
-let res1 = isPangram(test1);
-console.log(res1);
-let res2 = isPangram(test2);
-console.log(res2);
+console.log(res1, res2);
